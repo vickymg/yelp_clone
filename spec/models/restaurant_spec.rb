838bd0b74af218197dc5ad93d54c1651fc50e489
arnoldmanzano <arnold.manzano@gmail.com>
2016-02-29 17:07:43 +0000

    started associating reviews and restaurants

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Restaurant, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+describe Restaurant, type: :model do
+  it { is_expected.to have_many :reviews }
 end
