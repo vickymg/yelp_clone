c70e157668728ebf9bdabd8209207503f26ae407
Vicky Gray <vickygray26@hotmail.com>
2016-02-29 17:58:50 +0000

    added validations for uniqueness and reviews

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -12,4 +12,10 @@ describe Restaurant, type: :model do
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
   end
+
+  it 'is not valid unles it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
